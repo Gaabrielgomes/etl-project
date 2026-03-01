d9759c43398f8069a09706bac6a5f332f25b7c9d
Gaabrielgomes <gaabrielgomes3241@gmail.com>
2026-03-01 10:49:49 -0300

    Data transformer updated

--- app/transform/transformer.py
+++ app/transform/transformer.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
 
-def calculate_high_value_transactions(df: pd.DataFrame, treshold: float):
-    df["is_high_value"] = df["value"] > treshold
-    return df[df["is_high_value"] == True].copy()
+def get_high_value_transactions(df: pd.DataFrame, threshold: float) -> pd.DataFrame:
+    return df[df["value"] > threshold].copy()
 
 
-def add_month_column(df: pd.DataFrame):
-    df["month"] = df["date"].dt.to_period("M").dt.to_timestamp()
-    return df
+def add_month_column(df: pd.DataFrame) -> pd.DataFrame:
+    if "date" not in df.columns:
+        raise ValueError("DataFrame não possui coluna 'date'")
+    
+    df_result = df.copy()
+    df_result["month"] = df_result["date"].dt.to_period("M").dt.to_timestamp()
+    return df_result
